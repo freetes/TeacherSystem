73a286063fd4f1b884086d77a77ad33d024da886
daidaidongdong <31694949+daidaidongdong@users.noreply.github.com>
2018-02-06 13:43:58 +0800

    wj2.6

--- public/js/homeBySecretary.js
+++ public/js/homeBySecretary.js
@@ -30,7 +30,29 @@ function getAllInfo(){
 			
 		},
 		info=>{
-			console.log(info)
+			const users = info.user;
+			let trs1="";
+			for(let item of users){
+				trs1+=`<tr><td>${item.name}</td><td>${item.id}</td><td>${item.password}</td></tr>`
+			}
+			$("#userInfoTable").append(trs1);
+
+			const pay = info.pay;
+			let trs2="";
+			for(let item of pay){
+				if(item.isChecked==1){
+					trs2+=`<tr><td>${item.id}</td><td>${item.pay}</td><td>${item.isChecked}</td><td><button>待确认</button></td><td>${item.applySemester}</td><td>${item.applyDate}</td></tr>`
+				}
+				else if(item.isChecked==2){
+					trs2+=`<tr><td>${item.id}</td><td>${item.pay}</td><td>${item.isChecked}</td><td>已确认</td><td>${item.applySemester}</td><td>${item.applyDate}</td></tr>`	
+				}
+				else{
+					trs2+=`<tr><td>${item.id}</td><td>${item.pay}</td><td>${item.isChecked}</td><td>未上传</td><td>${item.applySemester}</td><td>${item.applyDate}</td></tr>`	
+				}
+			}
+			$("#payInfoTable").append(trs2);
 		}
 	)
 }
+
+
