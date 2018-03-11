e34e6c6927a9e08bb24ef0a01f4bc02f5a98d4df
lishuang <www.1030415864@qq.com>
2018-03-11 22:24:29 +0800

    fy

--- public/js/homeByUser.js
+++ public/js/homeByUser.js
@@ -56,6 +56,7 @@ function changePay(value){
       }
     )
 };
+
 // 提交审核
 function checkPay(value){
   $.post('/users/checkPay',
@@ -79,57 +80,58 @@ const getNewDate = ()=>{
 }
 
 // 点击事件
-const addNewClass = ()=>{
-  $(".addNewClassMessage").text("")
-  for(let item of $("input.publicInput")){
-    if(item.value==''){
-      $(".addNewClassMessage").text("必须全部填写")
-      return 
-    }
-  }
-  if($(".classKindSelect").val() == 'normal'){
-    for(let item of $(".normalClass").find('input.form-control')){
-      if(item.value==''){
-        $(".addNewClassMessage").text("必须全部填写")
-        return 
-      }
-    }
-  }
-  else if($(".classKindSelect").val() == 'design'){
-    for(let item of $(".designClass").find('input.form-control')){
-      if(item.value==''){
-        $(".addNewClassMessage").text("必须全部填写")
-        return 
-      }
-    }
-  }
-  else if($(".classKindSelect").val() == 'train'){
-    for(let item of $(".trainClass").find('input.form-control')){
-      if(item.value==''){
-        $(".addNewClassMessage").text("必须全部填写")
-        return 
-      }
-    }
-  }
-  else{
-    for(let item of $(".otherClass").find('input.form-control')){
-      if(item.value==''){
-        $(".addNewClassMessage").text("必须全部填写")
-        return 
-      }
-    }
-  }
+// const addNewClass = ()=>{
+//   $(".addNewClassMessage").text("")
+//   for(let item of $("input.publicInput")){
+//     if(item.value==''){
+//       $(".addNewClassMessage").text("必须全部填写")
+//       return 
+//     }
+//   }
+//   if($(".classKindSelect").val() == 'normal'){
+//     for(let item of $(".normalClass").find('input.form-control')){
+//       if(item.value==''){
+//         $(".addNewClassMessage").text("必须全部填写")
+//         return 
+//       }
+//     }
+//   }
+//   else if($(".classKindSelect").val() == 'design'){
+//     for(let item of $(".designClass").find('input.form-control')){
+//       if(item.value==''){
+//         $(".addNewClassMessage").text("必须全部填写")
+//         return 
+//       }
+//     }
+//   }
+//   else if($(".classKindSelect").val() == 'train'){
+//     for(let item of $(".trainClass").find('input.form-control')){
+//       if(item.value==''){
+//         $(".addNewClassMessage").text("必须全部填写")
+//         return 
+//       }
+//     }
+//   }
+//   else{
+//     for(let item of $(".otherClass").find('input.form-control')){
+//       if(item.value==''){
+//         $(".addNewClassMessage").text("必须全部填写")
+//         return 
+//       }
+//     }
+//   }
   
-  $(".addNewClassForm").submit()
-}
-const allClassClick = ()=>{
-  $(".homePageDiv").css("display", "none")
-  if($(".allClassDiv").css("display")=='none') $(".allClassDiv").slideToggle();
-}
-const addClassClick = ()=>{
-  $(".homePageDiv").css("display", "none")
-  if($(".addClassDiv").css("display")=='none') $(".addClassDiv").slideToggle();
-}
+//   $(".addNewClassForm").submit()
+// }
+// const allClassClick = ()=>{
+//   $(".homePageDiv").css("display", "none")
+//   if($(".allClassDiv").css("display")=='none') $(".allClassDiv").slideToggle();
+// }
+// const addClassClick = ()=>{
+//   $(".homePageDiv").css("display", "none")
+//   if($(".addClassDiv").css("display")=='none') $(".addClassDiv").slideToggle();
+// }
+
 const mainDivClick = ()=>{
   $(".homePageDiv").css("display", "none")
   if($(".mainDiv").css("display")=='none') $(".mainDiv").slideToggle();
@@ -140,61 +142,61 @@ const payDivClick = ()=>{
 }
 
 // 选择课程类别
-$(".classKindSelect").change(function(){
-  if(this.value == 'normal'){
-    $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">标准学时</span>
-        <input type="text" name="standardHours" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">理论学时</span>
-        <input type="text" name="theoryHours" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">实验学时</span>
-        <input type="text" name="experimentHours" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">合班系数</span>
-        <input type="text" name="point" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">实际学时</span>
-        <input type="text" name="finalHours" class="form-control"/>
-      </div>`)
-  }
-  else if(this.value == 'design'){
-    $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">周数</span>
-        <input type="text" name="week" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">实验学时</span>
-        <input type="text" name="experimentHours" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">合班系数</span>
-        <input type="text" name="point" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">实际学时</span>
-        <input type="text" name="finalHours" class="form-control"/>
-      </div>`)
-  }
-  else if(this.value == 'train'){
-    $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">单位学时</span>
-        <input type="text" name="unitHours" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">合班系数</span>
-        <input type="text" name="point" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">实际学时</span>
-        <input type="text" name="finalHours" class="form-control"/>
-      </div>`)
-  }
-  else{
-    $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">周数</span>
-        <input type="text" name="week" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">单位学时</span>
-        <input type="text" name="unitHours" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">合班系数</span>
-        <input type="text" name="point" class="form-control"/>
-      </div>
-      <div class="input-group"><span class="input-group-addon">实际学时</span>
-        <input type="text" name="finalHours" class="form-control"/>
-      </div>`)
-  }
-})
+// $(".classKindSelect").change(function(){
+//   if(this.value == 'normal'){
+//     $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">标准学时</span>
+//         <input type="text" name="standardHours" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">理论学时</span>
+//         <input type="text" name="theoryHours" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">实验学时</span>
+//         <input type="text" name="experimentHours" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">合班系数</span>
+//         <input type="text" name="point" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">实际学时</span>
+//         <input type="text" name="finalHours" class="form-control"/>
+//       </div>`)
+//   }
+//   else if(this.value == 'design'){
+//     $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">周数</span>
+//         <input type="text" name="week" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">实验学时</span>
+//         <input type="text" name="experimentHours" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">合班系数</span>
+//         <input type="text" name="point" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">实际学时</span>
+//         <input type="text" name="finalHours" class="form-control"/>
+//       </div>`)
+//   }
+//   else if(this.value == 'train'){
+//     $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">单位学时</span>
+//         <input type="text" name="unitHours" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">合班系数</span>
+//         <input type="text" name="point" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">实际学时</span>
+//         <input type="text" name="finalHours" class="form-control"/>
+//       </div>`)
+//   }
+//   else{
+//     $(".classesInputGroup").html(`<div class="input-group"><span class="input-group-addon">周数</span>
+//         <input type="text" name="week" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">单位学时</span>
+//         <input type="text" name="unitHours" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">合班系数</span>
+//         <input type="text" name="point" class="form-control"/>
+//       </div>
+//       <div class="input-group"><span class="input-group-addon">实际学时</span>
+//         <input type="text" name="finalHours" class="form-control"/>
+//       </div>`)
+//   }
+// })
