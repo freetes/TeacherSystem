7dce309025966ee1d76eb6d2a859f7592f3a075c
lishuang <www.1030415864@qq.com>
2018-03-28 20:35:10 +0800

    aa

--- model/dataModel.js
+++ model/dataModel.js
@@ -67,99 +67,125 @@ const NoticeSchema = new Schema({
 });
 
 // 课程模式
-const ClassSchema = new Schema({
-  id: String,
-  
-  name: String,
-  teacher: {      // 教师信息
+const CourseSchema = new Schema({
+  id: String,   // 课程号
+  name: String, // 课程名称
+  time: String, // 上课时间
+  place: String,// 上课地点
+  number: Number, // 班级人数
+  kind: String, // 课程种类
+  dateInfo: {
+    year: String,     // 学年
+    semester: Number, // 学期
+    dayOfWeek: Number,// 星期几
+    beginWeek: String,// 起始周
+    classTime: String // 上课节次
+  },
+  teacherInfo: {      // 教师信息
     id: String,
     name: String,
     telephone: String,
     department: String,
-    
+    gender: String,
+    education: String,
+    department: String
   },
-  classroom: {    // 教室信息
+  classroomInfo: {    // 教室信息
+    id: String,       // 场地编号
+    name: String,     // 场地名称
+    kind: String,     // 教学楼
+    seats: Number, // 总座位数
+    beginWeek: String,// 起始周
+    classTime: String, // 上课节次
+    classComposition: String, // 教学班组成
+    campus: String     // 校区
+  },
+  chooseInfo: {       // 选课信息
     id: String,
-    name: String,
-    kind: String
+    department: String,
+    credit: Number,
+    hour: Number,
+    number: Number,
   }
 })
 
-// 普通课程模式
-const NormalClassSchema = new Schema({
-  id: String,               // 工号
-  semester: String,         // 学期
-  name: String,             // 课程名
-  faculty: String,          // 专业
-  class: String,            // 班级
-  studentAmount: Number,    // 人数
-  standardHours: Number,    // 标准学时
-  theoryHours: Number,      // 理论学时
-  experimentHours: Number,  // 实验学时
-  finalHours: Number,       // 实际学时
-  date: String,             // 添加/修改日期
-  point: Number,            // 课程权重
-  isChecked: Boolean        // 是否审核
-});
-// 课程设计模式
-const DesignClassSchema = new Schema({
-  id: String,               // 工号
-  semester: String,         // 学期
-  name: String,             // 课程名：课程设计
-  faculty: String,
-  class: String,
-  studentAmount: Number,
-  week: Number,             // 周数
-  experimentHours: Number,  // 实验学时
-  finalHours: Number,        // 实际学时
-  date: String,             // 添加/修改日期
-  point: Number,            // 课程权重
-  isChecked: Boolean        // 是否审核
-});
-// 科研训练模式
-const TrainClassSchema = new Schema({
-  id: String,
-  semester: String, 
-  name: String,
-  faculty: String,
-  class: String,
-  studentAmount: Number,
-  unitHours: Number,        // 单位学时
-  finalHours: Number,       // 实际学时
-  date: String,             // 添加/修改日期
-  point: Number,            // 课程权重
-  isChecked: Boolean        // 是否审核
-});
-// 生产实习模式
-const ProduceClassSchema = new Schema({
-  id: String,
-  semester: String, 
-  name: String,
-  faculty: String,
-  class: String,
-  studentAmount: Number,
-  week: Number,             // 周数
-  unitHours: Number,        // 单位学时
-  finalHours: Number,       // 实际学时
-  date: String,             // 添加/修改日期
-  point: Number,            // 课程权重
-  isChecked: Boolean        // 是否审核
-});
-// 毕业实习模式
-const GraduateClassSchema = new Schema({
-  id: String,
-  semester: String, 
-  name: String,
-  faculty: String,
-  class: String,
-  studentAmount: Number,
-  week: Number,
-  unitHours: Number,
-  finalHours: Number,
-  date: String,             // 添加/修改日期
-  point: Number,            // 课程权重
-  isChecked: Boolean        // 是否审核
-});
+/*
+  // 普通课程模式
+  const NormalClassSchema = new Schema({
+    id: String,               // 工号
+    semester: String,         // 学期
+    name: String,             // 课程名
+    faculty: String,          // 专业
+    class: String,            // 班级
+    studentAmount: Number,    // 人数
+    standardHours: Number,    // 标准学时
+    theoryHours: Number,      // 理论学时
+    experimentHours: Number,  // 实验学时
+    finalHours: Number,       // 实际学时
+    date: String,             // 添加/修改日期
+    point: Number,            // 课程权重
+    isChecked: Boolean        // 是否审核
+  });
+  // 课程设计模式
+  const DesignClassSchema = new Schema({
+    id: String,               // 工号
+    semester: String,         // 学期
+    name: String,             // 课程名：课程设计
+    faculty: String,
+    class: String,
+    studentAmount: Number,
+    week: Number,             // 周数
+    experimentHours: Number,  // 实验学时
+    finalHours: Number,        // 实际学时
+    date: String,             // 添加/修改日期
+    point: Number,            // 课程权重
+    isChecked: Boolean        // 是否审核
+  });
+  // 科研训练模式
+  const TrainClassSchema = new Schema({
+    id: String,
+    semester: String, 
+    name: String,
+    faculty: String,
+    class: String,
+    studentAmount: Number,
+    unitHours: Number,        // 单位学时
+    finalHours: Number,       // 实际学时
+    date: String,             // 添加/修改日期
+    point: Number,            // 课程权重
+    isChecked: Boolean        // 是否审核
+  });
+  // 生产实习模式
+  const ProduceClassSchema = new Schema({
+    id: String,
+    semester: String, 
+    name: String,
+    faculty: String,
+    class: String,
+    studentAmount: Number,
+    week: Number,             // 周数
+    unitHours: Number,        // 单位学时
+    finalHours: Number,       // 实际学时
+    date: String,             // 添加/修改日期
+    point: Number,            // 课程权重
+    isChecked: Boolean        // 是否审核
+  });
+  // 毕业实习模式
+  const GraduateClassSchema = new Schema({
+    id: String,
+    semester: String, 
+    name: String,
+    faculty: String,
+    class: String,
+    studentAmount: Number,
+    week: Number,
+    unitHours: Number,
+    finalHours: Number,
+    date: String,             // 添加/修改日期
+    point: Number,            // 课程权重
+    isChecked: Boolean        // 是否审核
+  });
+*/
 
 //数据模型
 const Models = {
@@ -168,11 +194,7 @@ const Models = {
   FeedbackModel: mongoose.model('Feedback', FeedbackSchema),
   SigninLogModel: mongoose.model('SigninLog', SigninLogSchema),
   NoticeModel: mongoose.model('Notice', NoticeSchema),
-  NormalClassModel: mongoose.model('NormalClass', NormalClassSchema),
-  DesignClassSchema: mongoose.model('DesignClass', DesignClassSchema),
-  TrainClassSchema: mongoose.model('TrainClass', TrainClassSchema),
-  ProduceClassSchema: mongoose.model('ProduceClass', ProduceClassSchema),
-  GraduateClassSchema: mongoose.model('GraduateClass', GraduateClassSchema)
+  CourseModel: mongoose.model('Course', CourseSchema)
 };
 
 module.exports = Models;
